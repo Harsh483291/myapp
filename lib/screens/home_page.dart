b895313568affe589f1e5f42c857365cc50742f9
Harsh483291 <hk555697@gmail.com>
2025-04-09 21:10:51 +0000

    update

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -14,9 +12,11 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final FirebaseFirestore db = FirebaseFirestore.instance;
-  //creat an empty list of maps which represent our tasks
+
+  //Create an empty list of maps which represent our tasks
   final List<Map<String, dynamic>> tasks = [];
-  // Create a variable that captures the input of a text input
+
+  //Create a variable that captures the input of a text input
   final TextEditingController nameController = TextEditingController();
 
   @override
@@ -43,16 +43,49 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
-  //function add new task to local state & firestore database
+  //function add new tasks to local state & firestore database
   Future<void> addTask() async {
     final taskName = nameController.text.trim();
+
     if (taskName.isNotEmpty) {
       final newTask = {
         'name': taskName,
         'completed': false,
         'timestamp': FieldValue.serverTimestamp(),
       };
+
+      //docRef gives us the insertion id from the document
+
+      final docRef = await db.collection('tasks').add(newTask);
+
+      // add the tasks locally
+      setState(() {
+        tasks.add({'id': docRef.id, ...newTask});
+      });
+      nameController.clear();
     }
+  }
+
+  //Update the completion status of the task in Firestore & locally
+  Future<void> updateTask(int index, bool completed) async {
+    final task = tasks[index];
+    await db.collection('tasks').doc(task['id']).update({
+      'completed': completed,
+    });
+
+    setState(() {
+      task['completed'] = completed;
+    });
+  }
+
+  //delete the task from Firestore & locally
+  Future<void> removeTasks(int index) async {
+    final task = tasks[index];
+    await db.collection('tasks').doc(task['id']).delete();
+
+    setState(() {
+      tasks.removeAt(index);
+    });
   }
 
   @override
@@ -68,23 +101,32 @@ class _HomePageState extends State<HomePage> {
               'RDP Daily Planner',
               style: TextStyle(
                 fontFamily: 'Caveat',
-                color: Colors.white,
                 fontSize: 32,
+                color: Colors.white,
               ),
             ),
           ],
         ),
       ),
-      body: Column(
-        children: [
-          TableCalendar(
-            calendarFormat: CalendarFormat.month,
-            focusedDay: DateTime.now(),
-            firstDay: DateTime(2025),
-            lastDay: DateTime(2026),
-          ),
-          buildAddTaskSection(nameController),
-        ],
+      body: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 340,
+              child: TableCalendar(
+                calendarFormat: CalendarFormat.month,
+                focusedDay: DateTime.now(),
+                firstDay: DateTime(2025),
+                lastDay: DateTime(2026),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                child: buildAddTaskSection(nameController, addTask),
+              ),
+            ),
+          ],
+        ),
       ),
       drawer: Drawer(),
     );
@@ -92,13 +134,12 @@ class _HomePageState extends State<HomePage> {
 }
 
 //Build the section for adding tasks
-Widget buildAddTaskSection(nameController) {
+Widget buildAddTaskSection(nameController, addTask) {
   return Padding(
     padding: const EdgeInsets.all(12.0),
     child: Row(
       children: [
-        Container(
-          decoration: BoxDecoration(color: Colors.white),
+        Expanded(
           child: TextField(
             maxLength: 32,
             controller: nameController,
@@ -108,20 +149,42 @@ Widget buildAddTaskSection(nameController) {
             ),
           ),
         ),
-        ElevatedButton(onPressed: null, child: Text('Add Task')),
+        ElevatedButton(onPressed: addTask, child: Text('Add Task')),
       ],
     ),
   );
 }
 
-Widget buildTaskList(tasks) {
+Widget buildTaskList(tasks, updateTask, removeTasks) {
   return ListView.builder(
     physics: NeverScrollableScrollPhysics(),
-    shrinkWrap: true,
     itemCount: tasks.length,
     itemBuilder: (context, index) {
+      final task = tasks[index];
       return ListTile(
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+        leading: Icon(
+          task['completed'] ? Icons.check_circle : Icons.circle_outlined,
+        ),
+        title: Text(
+          task['name'],
+          style: TextStyle(
+            decoration: task['completed'] ? TextDecoration.lineThrough : null,
+            fontSize: 22,
+          ),
+        ),
+        trailing: Row(
+          children: [
+            Checkbox(
+              value: task['completed'],
+              onChanged: (value) => updateTask(index, value!),
+            ),
+            IconButton(
+              icon: Icon(Icons.delete),
+              onPressed: () => removeTasks(index),
+            ),
+          ],
+        ),
       );
     },
   );
